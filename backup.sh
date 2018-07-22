e82d0498778ebf759e5225fe207c533fe9542358
Tatsuya Sakamoto <pgm.gonta@gmail.com>
2018-07-22 13:30:34 +0900

    fix

--- backup.sh
+++ backup.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# スナップショットの有効期限
+SNAPSHOT_EXPIRATION=$1
 
 print_log() {
   local timestamp=$(date '+%Y/%m/%d %H:%M:%S')
@@ -6,36 +9,27 @@ print_log() {
   echo "${timestamp} ${msg}"
 }
 
-without_dquote() {
-  sed -e "s/\"//g"
-}
-
+CURRENT_TIME=$(date '+%Y%m%d%H%M%S')
 PJ_LIST=$(cat ./pj_list)
+
 for pj in $PJ_LIST
 do
-  # 既存のスナップショットの削除
-  print_log "Start to delete snapshots in ${pj}"
-  snapshots=$(gcloud --project ${pj} --format=json compute snapshots list | jq -r ".[].name")
-  for snapshot in $snapshots
-  do
-    echo "Start to delete snapshot ${pj} ${snapshot}"
-    gcloud --project ${pj} -q --format=json compute snapshots delete ${snapshot}
-    echo "Deleted snapshot ${pj} ${snapshot} "
-  done
-  print_log "Deleted all snapshots in ${pj}"
-
   # スナップショットの生成
   print_log "Start to create GCE Instance Snapshots in ${pj}"
-  disks=$(gcloud --project $pj --format=json compute disks list | jq -r ".[] | {name: .name, zone: .zone} | [.name, .zone] | @csv")
-
-  printf '%s\n' "$disks" |
-  while IFS=, read -r diskname zone 
-  do
-    diskname=$(echo ${diskname} | without_dquote)
-    zone=$(echo ${zone##*/} | without_dquote)
+  gcloud --project $pj --format=json compute disks list --format='value(name,zone)'| while read diskname zone; do 
     print_log "Start to create snapshot: ${pj} ${diskname} ${zone}"
-    gcloud --project $pj compute disks snapshot $diskname --zone=$zone
+    gcloud --project $pj compute disks snapshot $diskname --snapshot-names auto-bk-${diskname}-${CURRENT_TIME} --zone=$zone
     print_log "Created snapshot: ${pj} ${disk}"
   done
-done
 
+  #  有効期限が切れたスナップショットの削除
+  print_log "Start to delete snapshots in ${pj}"
+  from_date=$(date -d "${SNAPSHOT_EXPIRATION} day ago" '+%Y-%m-%d')
+  gcloud --project $pj compute snapshots list --format='value(name,creationTimestamp)' --filter="creationTimestamp <= $from_date AND name ~ '^auto-bk-*'" | while read snapshot creationTimestamp; do
+    echo "Start to delete snapshot ${pj} ${snapshot}"
+    gcloud --project ${pj} -q --format=json compute snapshots delete ${snapshot} 
+    echo "Deleted snapshot ${pj} ${snapshot} "
+  done
+  print_log "Completed to delete snapshots in ${pj}"
+
+done
